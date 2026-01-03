30421dbc2824118238328f2ebd11ab5368805bba
praniil <pparajuli15@gmail.com>
2026-01-03 22:22:35 +0545

    feat: initialized the autoencoder model and wrote the forward pass function

---
+++ src/autoencoder.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch
+
+class AutoEncoder(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.encoder_hidden_layer = nn.Linear(
+            in_features=kwargs["input_shape"], 
+            out_features=128
+        )
+        self.encoder_output_layer = nn.Linear(
+            in_features=128, 
+            out_features=128
+        )
+        self.decoder_hidden_layer = nn.Linear(
+            in_features=128,
+            out_features=128
+        )
+        self.decoder_output_layer = nn.Linear(
+            in_features=128,
+            out_features=kwargs["input_shape"]
+        )
+
+    def forward(self, features):
+        activation = self.encoder_hidden_layer(features)
+        activation = torch.relu(activation)
+        encode = self.encoder_output_layer(activation)
+        encode = torch.relu(encode)
+        activation = self.decoder_hidden_layer(encode)
+        activation = torch.relu(activation)
+        activation = self.decoder_output_layer(activation)
+        reconstructed = torch.relu(activation)
+        return reconstructed
+
