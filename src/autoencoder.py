442df9d66affcdeb951d2b58a453ba4c60f426bb
praniil <pparajuli15@gmail.com>
2026-01-05 08:36:35 +0545

    feat: added training loop of the auto encoder

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -4,6 +4,10 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
+# hyper-params
+batch_size = 512
+epochs = 20
+learning_rate = 1e-5
 class AutoEncoder(nn.Module):
     def __init__(self, **kwargs):
         super().__init__()
@@ -44,7 +48,7 @@ model = model.to(device=device)
 
 # define an optimizer
 # Adam optimizer 
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # define a costfunction
 craiterion = nn.MSELoss()
@@ -61,9 +65,39 @@ test_dataset = torchvision.datasets.MNIST(
 )
 
 train_loader = torch.utils.data.DataLoader(
-    dataset=train_dataset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True
+    dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True
 )
 
 test_loader = torch.utils.data.DataLoader(
     dataset= test_dataset, batch_size= 32, shuffle= False, num_workers=4
 )
+
+# training the network
+for epoch in range(epochs):
+    loss = 0
+    for batch_features, _ in train_loader:  #labels are ignored _ used
+        batch_features = batch_features.view(-1, 784).to(device)
+
+        #reset the gradients back to zero
+        optimizer.zero_grad()
+
+        # compute reconstructions
+        outputs = model(batch_features)
+
+        # compute loss
+        train_loss = craiterion(batch_features, outputs)
+
+        # compute accumulated gradients
+        train_loss.backward()
+        
+        # parameter update based on current gradients
+        optimizer.step()
+
+        # mini-batch training loss and epoch loss added
+        loss += train_loss.item()
+    
+    #compute the epoch training loss
+    loss = loss / len(train_loader)
+
+    # display the epoch training loss
+    print("epoch : {}/{}, recon loss = {:.8f}".format(epoch + 1, epochs, loss))    
